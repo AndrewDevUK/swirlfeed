93b0868c2dafa0735e5f22fcbbf611f87db6c026
Andrew Morgan <acm_1987@hotmail.com>
2022-10-09 14:03:40 +0100

    Created database conncection in index

--- public/index.php
+++ public/index.php
@@ -1,5 +1,25 @@
 <?php
+$server = 'mysql';
+$db = 'swirlfeed';
+$username = 'andrew';
+$password = 'password';
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
 
-echo "Hello from PHP";
+$pdo = new PDO("mysql:dbname=$db;host=$server", $username, $password, $options);
+?>
 
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Swirlfeed</title>
+</head>
+<body>
+
+</body>
+</html>
