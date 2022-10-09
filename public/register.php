181f49ab3939316217ceadc77e6047c37b1e8c78
Andrew Morgan <acm_1987@hotmail.com>
2022-10-09 14:18:02 +0100

    Created register.php and added skeleton of form

---
+++ public/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Swirlfeed</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="reg_fname" placeholder="First Name" required>
+        <br>
+        <input type="text" name="reg_lname" placeholder="Last Name" required>
+        <br>
+        <input type="email" name="reg_email" placeholder="Email" required>
+        <br>
+        <input type="email" name="reg_email2" placeholder="Confirm Email" required>
+        <br>
+        <input type="password" name="reg_password" placeholder="Password" required>
+        <br>
+        <input type="password" name="reg_password2" placeholder="Confirm Password" required>
+    </form>
+</body>
+</html>
